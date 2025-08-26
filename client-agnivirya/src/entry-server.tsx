573db13aaa6f9f54f784df80339449d6f8cd52a6
Vijay Dhakad <vijay1997dhakad@gmail.com>
2025-08-26 19:30:29 +0530

    npm fix

--- client-agnivirya/src/entry-server.tsx
+++ client-agnivirya/src/entry-server.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import App from './App'
 
-export function render(url: string, initialState: any = {}) {
+export function render(_url: string, initialState: any = {}) {
   const app = React.createElement(App, { initialState })
   const html = ReactDOMServer.renderToString(app)
   
